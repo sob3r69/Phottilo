3f27b3cf4b19e70471f73791f0cf7e9dc7d8f993
sob3r69 <iamnotsob3r@gmail.com>
2023-04-25 17:37:19 +0300

    added functionality to move and zoom selected image with Konva library

--- front/src/components/ImageField/ImageField.tsx
+++ front/src/components/ImageField/ImageField.tsx
@@ -1,11 +1,25 @@
-import { useEffect, useState } from 'react';
-
+import { useEffect, useMemo, useRef, useState } from 'react';
+import './style.css';
+import { Layer, Rect, Stage, Image } from 'react-konva';
+import Konva from 'konva';
+import useImage from 'use-image';
+import { KonvaEventObject } from 'konva/lib/Node';
 interface ImageFieldProps {
   selectedImage: File;
 }
 
 export function ImageField({ selectedImage }: ImageFieldProps) {
   const [imageURL, setImageURLimageURL] = useState(String);
+  const [image] = useImage(imageURL);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  const [stage, setStage] = useState({
+    scale: 1,
+    x: 0,
+    y: 0,
+  });
+
+  var imageWrapper = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (selectedImage) {
@@ -13,13 +27,50 @@ export function ImageField({ selectedImage }: ImageFieldProps) {
     }
   }, [selectedImage]);
 
+  useEffect(() => {
+    if (imageWrapper.current?.offsetHeight && imageWrapper.current?.offsetWidth) {
+      setDimensions({
+        width: imageWrapper.current.offsetWidth,
+        height: imageWrapper.current.offsetHeight,
+      });
+    }
+  }, []);
+
+  const handleWheel = (e: KonvaEventObject<WheelEvent>) => {
+    e.evt.preventDefault();
+
+    const scaleBy = 1.2;
+    const stage = e.target.getStage()!;
+    const oldScale = stage!.scaleX();
+    const mousePointTo = {
+      x: stage.getPointerPosition()!.x / oldScale - stage.x() / oldScale,
+      y: stage.getPointerPosition()!.y / oldScale - stage.y() / oldScale,
+    };
+
+    const newScale = e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+    setStage({
+      scale: newScale,
+      x: (stage.getPointerPosition()!.x / newScale - mousePointTo.x) * newScale,
+      y: (stage.getPointerPosition()!.y / newScale - mousePointTo.y) * newScale,
+    });
+  };
+
   return (
-    <div className="image-field">
-      <img
-        className="selected-image"
-        src={imageURL}
-        alt="There is gonna be an image"
-      ></img>
+    <div className="image-field" ref={imageWrapper}>
+      <Stage
+        width={dimensions.width}
+        height={dimensions.height}
+        onWheel={handleWheel}
+        scaleX={stage.scale}
+        scaleY={stage.scale}
+        x={stage.x}
+        y={stage.y}
+      >
+        <Layer>
+          <Image image={image} draggable={true} />
+        </Layer>
+      </Stage>
     </div>
   );
 }
