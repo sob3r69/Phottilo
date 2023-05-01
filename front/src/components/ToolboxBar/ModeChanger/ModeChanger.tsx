834e6e94e575ad4bc66c10c8ead089efd9c27156
sob3r69 <iamnotsob3r@gmail.com>
2023-05-01 17:40:41 +0300

    a little bit organized ModChanger and Toolbox

--- front/src/components/ToolboxBar/ModeChanger/ModeChanger.tsx
+++ front/src/components/ToolboxBar/ModeChanger/ModeChanger.tsx
@@ -1,14 +1,38 @@
+import { Mode, modes } from '../../Modes/ModeTypes';
 import './style.css';
 
-export default function ModeChanger() {
+interface ModeChangerProps {
+  setMode: React.Dispatch<React.SetStateAction<Mode>>;
+  selectedMode: Mode;
+}
+
+export default function ModeChanger({ selectedMode, setMode }: ModeChangerProps) {
   return (
-    <div className="dropdown">
-      <span className="selected-mode">Paint Mode</span>
+    <div className="mode-changer-dropdown">
+      <span className="selected-mode">{selectedMode.name}</span>
       <div className="dropdown-content">
         <div className="modes-container">
-          <button>Resize Mode</button>
-          <button>Filter Mode</button>
-          <button>Hello</button>
+          <button
+            onClick={() => {
+              setMode(modes.paint);
+            }}
+          >
+            Paint Mode
+          </button>
+          <button
+            onClick={() => {
+              setMode(modes.filter);
+            }}
+          >
+            Filter Mode
+          </button>
+          <button
+            onClick={() => {
+              setMode(modes.resize);
+            }}
+          >
+            Resize Mode
+          </button>
         </div>
       </div>
     </div>
--- front/src/components/ToolboxBar/Toolbox/Toolbox.tsx
+++ front/src/components/ToolboxBar/Toolbox/Toolbox.tsx
@@ -14,34 +14,7 @@ interface ToolBoxProps {
 export default function Toolbox({ selectedMode, setMode }: ToolBoxProps) {
   return (
     <div className="toolbox">
-      <div className="dropdown">
-        <span className="selected-mode">{selectedMode.name}</span>
-        <div className="dropdown-content">
-          <div className="modes-container">
-            <button
-              onClick={() => {
-                setMode(modes.paint);
-              }}
-            >
-              Paint Mode
-            </button>
-            <button
-              onClick={() => {
-                setMode(modes.filter);
-              }}
-            >
-              Filter Mode
-            </button>
-            <button
-              onClick={() => {
-                setMode(modes.resize);
-              }}
-            >
-              Resize Mode
-            </button>
-          </div>
-        </div>
-      </div>
+      <ModeChanger selectedMode={selectedMode} setMode={setMode} />
       <div className="toolbox-separator"></div>
       <ToolButton Icon={BsBrush} />
       <ToolButton Icon={BsEraser} />
