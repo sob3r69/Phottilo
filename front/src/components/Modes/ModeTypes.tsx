f125d4394c1f8269ce2bd7a9da294db3cf2ca154
sob3r69 <iamnotsob3r@gmail.com>
2023-05-11 12:59:52 +0300

    fixed issue with modechanger

--- front/src/components/Modes/ModeTypes.tsx
+++ front/src/components/Modes/ModeTypes.tsx
@@ -1,19 +1,16 @@
 import FilterTools from './FilterMode/FilterTools';
-import PaintParams from './PaintMode/PaintParams';
 import PaintTools from './PaintMode/PaintTools';
 import './style.css';
 
 export type Mode = {
   name: string;
   tools: JSX.Element;
-  params: JSX.Element;
 };
 
 export const modes = {
   paint: {
     name: 'Paint Mode',
     tools: <PaintTools />,
-    params: <PaintParams />,
   },
   filter: {
     name: 'Filter Mode',
@@ -22,6 +19,5 @@ export const modes = {
   resize: {
     name: 'Resize Mode',
     tools: 'resize',
-    params: 'resize',
   },
 };
